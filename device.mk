0e707c7cfca9d59d14f65e1e02b8b1c453abd38c
millerliang <millerliang@google.com>
2022-10-21 15:25:52 +0800

    audio:Enable AAudio support
    Enable the realted property and the needed files
    for MMAP use cases.
    
    Bug: 254160121
    Test: OboeTester
    
    Signed-off-by: millerliang <millerliang@google.com>
    Change-Id: I17011349c80ac66601ccccd3edcac5aa529d2df9

--- device.mk
+++ device.mk
@@ -440,9 +440,9 @@ include device/google/zuma/aoc/device.mk
 #USE_XML_AUDIO_POLICY_CONF := 1
 
 # Enable AAudio MMAP/NOIRQ data path.
-#PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_policy=2
-#PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_exclusive_policy=2
-#PRODUCT_PROPERTY_OVERRIDES += aaudio.hw_burst_min_usec=2000
+PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_policy=2
+PRODUCT_PROPERTY_OVERRIDES += aaudio.mmap_exclusive_policy=2
+PRODUCT_PROPERTY_OVERRIDES += aaudio.hw_burst_min_usec=2000
 
 # Calliope firmware overwrite
 #PRODUCT_COPY_FILES += \
