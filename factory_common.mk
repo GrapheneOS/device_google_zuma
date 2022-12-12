86ba371f178ca7e0e4f14bf0d69713a868ad54c1
Gary Lin <garylse@google.com>
2022-12-12 11:43:58 +0800

    camera: disable scene detection for factory build
    Test: build pass, camera preview
    Bug: 261820013
    Change-Id: Ieb6142e48c36cc5a23977b3edf7914d45929d957

--- factory_common.mk
+++ factory_common.mk
@@ -44,7 +44,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.debug.force_segmentation_node_on=0 \
     vendor.camera.debug.enable_saliency=0 \
     vendor.camera.debug.force_local_tone_mapping_on=0 \
-    vendor.camera.debug.local_tone_mapping_controller_v1.mode=0
+    vendor.camera.debug.local_tone_mapping_controller_v1.mode=0 \
+    vendor.camera.debug.enable_scene_detection=0
 
 # Disable ScreenDecorations for factory builds
 PRODUCT_PROPERTY_OVERRIDES += \
