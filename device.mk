9d79164804c5878d53f96c060429bbc83e0d1fdd
Sajid Dalvi <sdalvi@google.com>
2023-01-24 18:56:37 -0600

    init: add dumpstate for pcie
    Add logbuffer support to pcie driver to reduce dmesg spam.
    
    Bug: 228870318
    Change-Id: I714110a83bd04972db1017fae7dd08ea61b7a31c
    Signed-off-by: Sajid Dalvi <sdalvi@google.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ include device/google/gs-common/soc/soc.mk
 include device/google/gs-common/modem/modem.mk
 include device/google/gs-common/aoc/aoc.mk
 include device/google/gs-common/trusty/trusty.mk
+include device/google/gs-common/pcie/pcie.mk
 include device/google/gs-common/storage/storage.mk
 include device/google/gs-common/thermal/thermal.mk
 
