3ba817ae3f587687a0a1934b65ab8f804db1f977
Kenny Root <kroot@google.com>
2023-02-27 14:23:00 -0800

    dumpstate: add GSA logs to bugreport
    Add support for reading the GSA logs sysfs file to see the GSA logs in
    the bugreports we get from dogfooders.
    
    (cherry picked from commit 09cb4227bb313274e46b973024ed3820319778e1)
    
    Bug: 271125313
    Test: adb shell dumpstate
    Change-Id: I09a2e039451b5eb8b7fa360ba03486bd871fb970
    Merged-In: I09a2e039451b5eb8b7fa360ba03486bd871fb970

---
+++ dumpstate/dump_gsa.sh
@@ -0,0 +1,8 @@
+#!/vendor/bin/sh
+
+echo "--- GSA MAIN LOG"
+cat /sys/devices/platform/16490000.gsa-ns/log_main
+
+echo "--- GSA INTERMEDIATE LOG"
+cat /sys/devices/platform/16490000.gsa-ns/log_intermediate
+
