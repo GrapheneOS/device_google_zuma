6c257f53980f0d1d0c6cfb4cc82ad8ac02246f7c
Ziyi Cui <ziyic@google.com>
2023-03-12 23:35:34 +0000

    zuma: Update path reading for temp-residency-metrics
    Bug: 232541623
    
    Test: Verified the existence and correctness of atom
    
    adb shell cmd stats print-stats | grep 105045
    
    Change-Id: Id97f63503e71a6def173ef2debdb256b976bc2e8
    Signed-off-by: Ziyi Cui <ziyic@google.com>

--- pixelstats/service.cpp
+++ pixelstats/service.cpp
@@ -61,6 +61,10 @@ const struct SysfsCollector::SysfsPaths sysfs_paths = {
     .AmsRatePath = "/sys/devices/platform/audiometrics/ams_rate_read_once",
     .ResumeLatencyMetricsPath = "/sys/kernel/metrics/resume_latency/resume_latency_metrics",
     .LongIRQMetricsPath = "/sys/kernel/metrics/irq/long_irq_metrics",
+    .TempResidencyPaths = {
+            "/sys/kernel/metrics/thermal/tr_by_group/tmu/stats",
+            "/sys/kernel/metrics/thermal/tr_by_group/spmic/stats",
+    },
     .ModemPcieLinkStatsPath = "/sys/devices/platform/12100000.pcie/link_stats",
     .WifiPcieLinkStatsPath = "/sys/devices/platform/13120000.pcie/link_stats"
 };
