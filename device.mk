0ad2dcf1b1315fa75552e39f972be967c79377dc
Petri Gynther <pgynther@google.com>
2023-06-27 17:18:16 -0700

    Revert "Disable DP on all images"
    This reverts commit 9beb8589996db37841580ba409bd502325673182.
    
    Re-enable DP on udc-qpr-dev.
    
    Bug: 288506710
    Change-Id: Iee772dfa1f41b235fc0aa9f024dca1dc58085f16

--- device.mk
+++ device.mk
@@ -205,7 +205,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.vendor.usb.displayport.enabled=0
+	persist.vendor.usb.displayport.enabled=1
 else
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.usb.displayport.enabled=0
