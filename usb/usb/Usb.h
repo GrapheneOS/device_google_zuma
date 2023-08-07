a6462edbe394dedb2fcb04ebc9c0e65b7e2b8de0
Roy Luo <royluo@google.com>
2023-08-07 19:48:05 +0000

    Usb: Listen to USB sysfs attribute for device state
    Poll the sysfs attributes that represents usb device state in
    either of the following states: not attached, powered, default,
    addressed, configured.
    The information is useful in detecting non compliant USB cable,
    which will be supported in later patch sets. This patch lays
    the ground work to monitor the sysfs attributes in both device
    and host modes.
    The thread to poll uevent is re-used to poll sysfs because this
    serialize type-C port events and usb device state changes, hence
    prevent potential races.
    Added a thin abstration layer and a map to keep epoll data so
    that it's easier to dynamically add/delete files to epoll, which
    is needed for usb devices in host mode.
    
    Bug: 285199434
    Test: trigger usb device state changes in device and host mode
    Change-Id: Ie5389d051deb28dbb486c2f27319b3cc9e89312f

--- usb/usb/Usb.h
+++ usb/usb/Usb.h
@@ -121,6 +121,29 @@ struct Usb : public BnUsb {
     pthread_mutex_t mDisplayPortLock;
     // eventfd to signal DisplayPort thread
     int mDisplayPortEventPipe;
+
+    // USB device state monitoring
+    struct usbDeviceState {
+        std::string latestState;
+        int portResetCount;
+    };
+    struct usbDeviceState mDeviceState;
+    // Map host device path name to usbDeviceState
+    std::map<std::string, struct usbDeviceState> mHostStateMap;
+
+    // File monitoring through epoll
+    int mEpollFd;
+    struct payload {
+        int fd;
+        std::string name;
+        Usb *usb;
+     };
+    struct epollEntry {
+         struct payload payload;
+         std::function<void(uint32_t)> cb;
+    };
+    std::map<std::string, struct epollEntry> mEpollEntries;
+
   private:
     pthread_t mPoll;
     pthread_t mDisplayPortPoll;
