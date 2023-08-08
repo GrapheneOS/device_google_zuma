8a20d948813463df983d22d23aace4c1696950e0
Renato Grottesi <otaner@google.com>
2023-08-08 05:19:43 +0000

    Disable armnn nnapi backend
    Discontinue maintenance and active support of ArmNN on Pixel phones.
    
    Bug: 294459623
    Test: Local nnapi_info
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:06b3d4167756c4135837f64f9c8312a6d9a9dd14)
    Merged-In: Ib6140584a724b5e1fd3634e414773092da49b41a
    Change-Id: Ib6140584a724b5e1fd3634e414773092da49b41a

--- device.mk
+++ device.mk
@@ -244,8 +244,6 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call soong_config_set,pixel_mali,soc,$(TARGET_BOARD_PLATFORM))
 $(call soong_config_set,arm_gralloc,soc,$(TARGET_BOARD_PLATFORM))
 
-include device/google/gs-common/gpu/gpu.mk
-
 PRODUCT_PACKAGES += \
 	csffw_image_prebuilt__firmware_prebuilt_ttux_mali_csffw.bin \
 	libGLES_mali \
