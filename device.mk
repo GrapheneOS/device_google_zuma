77b0f3fc8c55f7cc934d380c6d295aa5f47ffc5f
Jing Wang <jwangh@google.com>
2023-09-28 21:03:10 +0000

    Revert "init.debug.rc: enable etm/etm2dram when SJTAG is unlocked"
    Revert submission 24754347-zuma-etm2dram
    
    Reason for revert: b/302352974
    
    Reverted changes: /q/submissionid:24754347-zuma-etm2dram
    
    Bug: 302352974
    Change-Id: I937658f7c78a97e2c3065a05058353b5cf16e860
    Signed-off-by: Jing Wang <jwangh@google.com>

--- device.mk
+++ device.mk
@@ -342,8 +342,7 @@ PRODUCT_COPY_FILES += \
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_COPY_FILES += \
-	device/google/zuma/conf/init.debug.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.debug.rc \
-	device/google/zuma/conf/init.check_ap_pd_auth.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.check_ap_pd_auth.sh
+	device/google/zuma/conf/init.debug.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.debug.rc
 PRODUCT_COPY_FILES += \
 	device/google/zuma/conf/init.freq.userdebug.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.freq.userdebug.rc
 endif
