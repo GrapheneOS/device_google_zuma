c2ffdd7dcd043e5454371f959946eb6501c656e9
maade69 <70593890+maade69@users.noreply.github.com>
2023-10-13 02:22:44 +0300

    (temporary) drop ImsMediaAoc

--- device.mk
+++ device.mk
@@ -890,15 +890,13 @@ PRODUCT_PACKAGES_DEBUG += \
 
 PRODUCT_PACKAGES += ShannonRcs
 
-ifeq (,$(filter aosp_% factory_%,$(TARGET_PRODUCT)))
 #ImsMediaAoc library
-FEATURE_TYPE := oem_audio
-SOONG_CONFIG_NAMESPACES += audio_lib
-SOONG_CONFIG_audio_lib += \
-        audio_type
+#FEATURE_TYPE := oem_audio
+#SOONG_CONFIG_NAMESPACES += audio_lib
+#SOONG_CONFIG_audio_lib += \
+#        audio_type
 
 SOONG_CONFIG_audio_lib_audio_type := $(FEATURE_TYPE)
-endif
 
 # ImsMedia
 PRODUCT_PACKAGES += \
