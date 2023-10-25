44082e91da7d9677c52aeaed79fa03e4c562c08f
Jaegeuk Kim <jaegeuk@google.com>
2023-10-25 12:31:27 -0700

    Run fsck to resolve possible data corruption
    Cherry-picked from 19eb98145d3d3abab65a25f75ccead8c562989f8.
    
    Trigger fsck on mount of /data if the value of ro.preventative_fsck is
    not equal to the contents of /metadata/vold/preventative_fsck, then set
    the file to the property to prevent future runs
    
    See b/305658663 for context
    
    Bug: 305658663
    Test: Make sure fsck run after first boot and not after second
    Change-Id: Ie6b9fc94752d91e77e8fc48979216917c1fd9ec3
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- device-common.mk
+++ device-common.mk
@@ -47,6 +47,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Set the max page size to 4096 (b/300367402)
 PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 4096
 
+# Trigger fsck on upgrade (305658663)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.preventative_fsck = 1
+
 # Indicate that the bootloader supports the MTE developer option switch
 # (MISC_MEMTAG_MODE_MEMTAG_ONCE), with the exception of _fullmte products that
 # force enable MTE.
