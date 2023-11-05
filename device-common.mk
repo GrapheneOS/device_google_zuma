f2d6acdf834e8daa4c18afc7e0a6775082d20933
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-05 21:05:13 +0200

    remove ro.arm64.memtag.bootctl_supported declaration
    Memory tagging support is enabled unconditionally on GrapheneOS, similar to a _fullmte build.
    
    Removal of this declaration hides both user-facing and developer-facing memory tagging settings.

--- device-common.mk
+++ device-common.mk
@@ -62,7 +62,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # eng products that force enable MTE
 ifeq (,$(filter %_fullmte,$(TARGET_PRODUCT)))
 ifeq (,$(filter eng,$(TARGET_BUILD_VARIANT)))
-PRODUCT_PRODUCT_PROPERTIES += ro.arm64.memtag.bootctl_supported=1
 SANITIZE_TARGET := $(strip $(SANITIZE_TARGET) memtag_heap)
 BOARD_KERNEL_CMDLINE += bootloader.pixel.MTE_FORCE_ON
 endif
