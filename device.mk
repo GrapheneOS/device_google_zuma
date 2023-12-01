65439a058c0699533438651fcd53fa4f1387d4e1
Sebastian Pickl <spickl@google.com>
2023-12-01 11:30:35 +0000

    Revert "zuma: include bootctrl_aidl.mk from gs-common"
    Revert submission 25477883-gs-common_bootctrl-aidl
    
    Reason for revert: breaking builds b/314240126
    
    Bug: 314240126
    
    Reverted changes: /q/submissionid:25477883-gs-common_bootctrl-aidl
    
    Change-Id: I126138b048512fe6b2bbb2d8463152974e8f7c0a

--- device.mk
+++ device.mk
@@ -40,7 +40,6 @@ include device/google/gs-common/widevine/widevine.mk
 include device/google/gs-common/sota_app/factoryota.mk
 include device/google/gs-common/misc_writer/misc_writer.mk
 include device/google/gs-common/gyotaku_app/gyotaku.mk
-include device/google/gs-common/bootctrl/bootctrl_aidl.mk
 
 include device/google/zuma/dumpstate/item.mk
 
