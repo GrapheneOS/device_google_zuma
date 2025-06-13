b66729fee69ead157f7f32d4c11e82cc4310e01d
maade93791 <70593890+maade69@users.noreply.github.com>
2025-06-13 04:07:35 +0300

    dumpstate/
    either not needed or generated by adevtool

--- dumpstate/dump_gsa.sh
+++
@@ -1,8 +0,0 @@
-#!/vendor/bin/sh
-
-echo "--- GSA MAIN LOG"
-cat /sys/devices/platform/16490000.gsa-ns/log_main
-
-echo "--- GSA INTERMEDIATE LOG"
-cat /sys/devices/platform/16490000.gsa-ns/log_intermediate
-
--- dumpstate/item.mk
+++
@@ -1,4 +0,0 @@
-PRODUCT_PACKAGES += dump_wlan.sh \
-		    dump_power
-
-PRODUCT_PACKAGES_DEBUG += dump_gsa.sh
