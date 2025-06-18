de6baaa32ab95372ba1eeabc546fb5e70aa93375
Fred Underwood <underwoodfred@proton.me>
2025-06-18 17:22:07 +1000

    remove FUSE property

--- device.mk
+++ device.mk
@@ -468,10 +468,6 @@ $(call soong_config_set,google_displaycolor,displaycolor_platform,zuma)
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Use FUSE passthrough
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.sys.fuse.passthrough.enable=true
-
 # Use /product/etc/fstab.postinstall to mount system_other
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.postinstall.fstab.prefix=/product
