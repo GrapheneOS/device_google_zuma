b2a4902d3a5be6a32608fc1dc473a3656d219981
Fred Underwood <underwoodfred@proton.me>
2025-06-18 17:22:07 +1000

    remove FUSE property

--- device.mk
+++ device.mk
@@ -472,10 +472,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Use FUSE passthrough
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.sys.fuse.passthrough.enable=true
-
 # Use /product/etc/fstab.postinstall to mount system_other
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.postinstall.fstab.prefix=/product
