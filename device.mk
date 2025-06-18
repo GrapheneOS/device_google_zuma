c931fde5351920f8baa2615daf38571cc64d521f
Fred Underwood <underwoodfred@proton.me>
2025-06-18 17:14:43 +1000

    remove unavailable trusty soong namespace

--- device.mk
+++ device.mk
@@ -464,10 +464,6 @@ PRODUCT_PACKAGES_DEBUG += \
    trusty_stats_test \
    trusty-coverage-controller \
 
-# Trusty Metrics Daemon
-PRODUCT_SOONG_NAMESPACES += \
-	vendor/google/trusty/common
-
 PRODUCT_PACKAGES += \
 	trusty_metricsd
 
