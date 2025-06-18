9c1ea267f1b000d4263112a957e2fe77c4cfb35e
Fred Underwood <underwoodfred@proton.me>
2025-06-18 17:42:20 +1000

    remove framework_watchdog properties

--- device.mk
+++ device.mk
@@ -578,11 +578,6 @@ PRODUCT_BUILD_PVMFW_IMAGE := true
 # Enable to build standalone vendor_kernel_boot image.
 PRODUCT_BUILD_VENDOR_KERNEL_BOOT_IMAGE := true
 
-# Enable watchdog timeout loop breaker.
-PRODUCT_PROPERTY_OVERRIDES += \
-	framework_watchdog.fatal_window.second=600 \
-	framework_watchdog.fatal_count=3
-
 # Enable zygote critical window.
 PRODUCT_PROPERTY_OVERRIDES += \
 	zygote.critical_window.minute=10
