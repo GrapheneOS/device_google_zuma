20f00c382977047efbd109406e8db8535b0f9654
Fred Underwood <underwoodfred@proton.me>
2025-06-18 16:42:32 +1000

    remove GPU properties

--- device.mk
+++ device.mk
@@ -178,10 +178,6 @@ PRODUCT_COPY_FILES += \
 
 #endif
 
-PRODUCT_VENDOR_PROPERTIES += \
-	ro.opengles.version=196610 \
-	graphics.gpu.profiler.support=true
-
 # b/295257834 Add HDR shaders to SurfaceFlinger's pre-warming cache
 PRODUCT_VENDOR_PROPERTIES += ro.surface_flinger.prime_shader_cache.ultrahdr=1
 
