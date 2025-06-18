494d38ef6946e604fe71d03687ccf997ed935f1b
Fred Underwood <underwoodfred@proton.me>
2025-06-18 17:42:36 +1000

    remove zygote properties

--- device.mk
+++ device.mk
@@ -578,10 +578,6 @@ PRODUCT_BUILD_PVMFW_IMAGE := true
 # Enable to build standalone vendor_kernel_boot image.
 PRODUCT_BUILD_VENDOR_KERNEL_BOOT_IMAGE := true
 
-# Enable zygote critical window.
-PRODUCT_PROPERTY_OVERRIDES += \
-	zygote.critical_window.minute=10
-
 # Suspend properties
 PRODUCT_PROPERTY_OVERRIDES += \
     suspend.short_suspend_threshold_millis=2000 \
