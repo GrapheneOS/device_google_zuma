be51ab735bb2110d919f5cef23114a4ac1fd725d
Fred Underwood <underwoodfred@proton.me>
2025-06-18 16:47:35 +1000

    remove unavailable package

--- device.mk
+++ device.mk
@@ -273,10 +273,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	checkpoint_gc
 
-# RPMB TA
-PRODUCT_PACKAGES += \
-	tlrpmb
-
 # Touch firmware
 #PRODUCT_COPY_FILES += \
 	device/google/zuma/firmware/touch/s6sy761.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/s6sy761.fw
