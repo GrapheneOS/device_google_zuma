f78a19b1ef4443c75e34878930e4f77100de0a01
Fred Underwood <underwoodfred@proton.me>
2025-06-18 16:27:35 +1000

    remove ril properties

--- device.mk
+++ device.mk
@@ -115,14 +115,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 	ro.logcat.compress=true
 endif
 
-# Enable Early Camping
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.vendor.ril.camp_on_earlier=1
-
-# Enable SET_SCREEN_STATE request
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.vendor.ril.enable_set_screen_state=1
-
 # Set the Bluetooth Class of Device
 # Service Field: 0x5A -> 90
 #    Bit 14: LE audio
