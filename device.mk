19412d761a2471d91ac27ca7a233cd68e8828ed0
Fred Underwood <underwoodfred@proton.me>
2025-06-18 16:44:30 +1000

    remove conf/ueventd.zuma.rc

--- device.mk
+++ device.mk
@@ -214,8 +214,7 @@ PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 
 # Init files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_KERNEL):kernel \
-	device/google/zuma/conf/ueventd.zuma.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+	$(LOCAL_KERNEL):kernel
 
 # Recovery files
 PRODUCT_COPY_FILES += \
