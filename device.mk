fa633d840e4e1449a0d9daae69174c09cd164262
Fred Underwood <underwoodfred@proton.me>
2025-06-18 16:27:13 +1000

    remove radio properties

--- device.mk
+++ device.mk
@@ -115,14 +115,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 	ro.logcat.compress=true
 endif
 
-# Enable reboot free DSDS
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.radio.reboot_on_modem_change=false
-
-# Configure DSDS by default
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.radio.multisim.config=dsds
-
 # Enable Early Camping
 PRODUCT_PRODUCT_PROPERTIES += \
 	persist.vendor.ril.camp_on_earlier=1
