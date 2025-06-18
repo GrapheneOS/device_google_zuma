284b7679638848021d31238f9b6893240d68e185
Fred Underwood <underwoodfred@proton.me>
2025-06-18 17:29:42 +1000

    remove unavailable aoc namespace

--- device.mk
+++ device.mk
@@ -540,10 +540,6 @@ else
 include device/google/gs-common/audio/hidl_zuma.mk
 endif
 
-## AoC soong
-PRODUCT_SOONG_NAMESPACES += \
-        vendor/google/whitechapel/aoc
-
 $(call soong_config_set,aoc,target_soc,$(TARGET_BOARD_PLATFORM))
 $(call soong_config_set,aoc,target_product,$(TARGET_PRODUCT))
 
