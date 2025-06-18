5314d8f1eee97b2c74c3e585222cbf7c6c8592ef
Fred Underwood <underwoodfred@proton.me>
2025-06-18 16:48:14 +1000

    remove comments

--- device.mk
+++ device.mk
@@ -273,9 +273,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	checkpoint_gc
 
-# Touch firmware
-#PRODUCT_COPY_FILES += \
-	device/google/zuma/firmware/touch/s6sy761.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/s6sy761.fw
 # Touch
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
